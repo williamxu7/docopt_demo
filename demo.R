93a29ca095d95c515453006b182c9573a7cd8b16
William Xu <yihangxu001@hotmail.com>
2020-11-19 16:58:54 -0800

    Add a main function

--- demo.R
+++ demo.R
@@ -8,10 +8,16 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
-print(opt$arg4)
+
+main <- function() {
+    print(opt)
+    print(typeof(opt))
+    print(opt$arg4)
+}
+
+main()
