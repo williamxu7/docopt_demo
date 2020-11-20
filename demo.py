be4f99043c5ad50e0f21f70d1817218afcd896d8
William Xu <yihangxu001@hotmail.com>
2020-11-19 17:52:29 -0800

    Add a main function

--- demo.py
+++ demo.py
@@ -13,6 +13,11 @@ Options:
 
 from docopt import docopt
 opt = docopt(__doc__)
-print(opt)
-print(type(opt))
-print(opt["--arg4"])
+
+def main():
+    print(opt)
+    print(type(opt))
+    print(opt["--arg4"])
+
+if __name__=="__main__":
+    main()
